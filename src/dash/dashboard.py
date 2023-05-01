7ed4de007eb4235d750f7c8aff76df0cc835bda2
Jan N. Piotraschke <jan-piotraschke@imac-von-jan.speedport.ip>
2023-05-01 15:34:48 +0200

    added callbacks

--- src/dash/dashboard.py
+++ src/dash/dashboard.py
@@ -3,6 +3,7 @@ import dash
 from dash import dash_table
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
 
 from .data import create_dataframe
 from .layout import html_layout
@@ -28,6 +29,14 @@ def init_dashboard(server):
     # Create Layout
     dash_app.layout = serve_layout
 
+    # Callback to update the output area with the selected option
+    @dash_app.callback(
+        Output("output-area", "children"),
+        [Input("dropdown-menu", "value")],
+    )
+    def update_output_area(selected_option):
+        return f"Selected option: {selected_option}"
+
     return dash_app.server
 
 def serve_layout():
@@ -37,6 +46,15 @@ def serve_layout():
     # Create Layout
     layout = html.Div(
         children=[
+            dcc.Dropdown(  # Add a dropdown menu
+                id="dropdown-menu",
+                options=[
+                    {"label": option, "value": option}
+                    for option in df["city"].unique()
+                ],
+                value="",
+            ),
+            html.Div(id="output-area"),
             dcc.Graph(
                 id="histogram-graph",
                 figure={
